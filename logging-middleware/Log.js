8d36e1991088176617fe5ccd62fa0c0910f75de4
Surada Sri Anjaneya <suradasrianjaneya1819@gmail.com>
2026-06-23 11:38:53 +0530

    Implemented Log Function

---
+++ logging-middleware/Log.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+
+const logapi = process.env.LOG_API;
+const token = process.env.API_TOKEN;
+
+async function Log(stack,level,packageName,message) {
+    try{
+        const res = await fetch(logapi,{
+            method : "POST",
+            headers : {
+                'content-type':"application/json",
+                'Authorization':`Bearer ${token}`
+            },
+            body:JSON.stringify({
+                stack,
+                level,
+                package:packageName,
+                message
+            })
+        });
+        const data = await res.json();
+        console.log("Log Created",data);
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+}
+
+module.exports = {
+    Log
+}
